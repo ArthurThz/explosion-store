aeb87b1ceac729b19f6fe7e9430e0bc5b7d43dce
Arthur Theodoro | Front End Developer <arthur.theodoro.sva@hotmail.com>
2023-01-31 15:37:01 -0300

    added form component aswell signup with email and password

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
-import Home from "./components/Routes/Home/home"
+import { Routes, Route } from 'react-router-dom';
 
-import { Routes, Route, } from 'react-router-dom'
-
-import NavBar from "./components/Routes/Navbar/navbar"
-import SignIn from "./components/Routes/Sign-in/sign-in"
+import Home from './components/Routes/Home/home';
+import NavBar from './components/Routes/Navbar/navbar';
+import SignIn from './components/Routes/Sign-in/sign-in';
 
 function App() {
-  const Shop = () => {
-    return <h1>Im shop</h1>
-  }
   return (
     <Routes>
-      <Route path="/" element={<NavBar />} >
+      <Route path="/" element={<NavBar />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
-
-  )
+  );
 }
 
-export default App
+export default App;
